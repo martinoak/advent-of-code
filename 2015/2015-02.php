4a99aea7ba9aab94681405dab2b14dfbc6900ded
Dub Martin <martin.dub@dek-cz.com>
2022-07-27 08:53:42 +0200

    2015-02 Second part

--- 2015/2015-02.php
+++ 2015/2015-02.php
@@ -1003,11 +1003,14 @@ $input = "
 
 $input = explode("\n", $input);
 $total = 0;
+$ribbon = 0;
 foreach ($input as $line) {
     $line = explode("x", $line);
     $l = $line[0];
     $w = $line[1];
     $h = $line[2];
     $total += 2 * (int)$l * (int)$w + 2 * (int)$w * (int)$h + 2 * (int)$h * (int)$l + min((int)$l * (int)$w, (int)$w * (int)$h, (int)$h * (int)$l);
+
+    $ribbon += (int)$l * (int)$w * (int)$h + min((int)$l + (int)$l + (int)$w + (int)$w, (int)$w + (int)$w + (int)$h + (int)$h, (int)$h + (int)$h + (int)$l + (int)$l);
 }
-echo $total."\n";
+echo $total."\n".$ribbon."\n";
