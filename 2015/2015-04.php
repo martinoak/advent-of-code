2240661b64bb8383d2e03121567f9e7380aa0cc4
Dub Martin <martin.dub@dek-cz.com>
2022-07-27 10:44:56 +0200

    2015-04 Second part (like really...)

--- 2015/2015-04.php
+++ 2015/2015-04.php
@@ -4,7 +4,7 @@ $password = "";
 $i = 0;
 while (strlen($password) < 8) {
     $hash = md5($input . $i);
-    if (str_starts_with($hash, "00000")) {
+    if (str_starts_with($hash, "000000")) {
         $password .= substr($hash, 5, strlen($hash) - 5);
     }
     $i++;
