81e22ac1453e1333632460ceab4ee08cd1167d54
Dub Martin <martin.dub@dek-cz.com>
2022-12-06 15:38:58 +0100

    2022-01 Second part

--- 2022/2022-01.php
+++ 2022/2022-01.php
@@ -5,10 +5,8 @@ $input = file_get_contents('inputs/2022-01.txt');
 $groups = explode("\n\n", $input);
 $max = 0;
 foreach ($groups as $group) {
-    $sum = array_sum(explode("\n", $group));
-    if ($sum > $max) {
-        $max = $sum;
-    }
+    $sum[] = array_sum(explode("\n", $group));
 }
-
-echo $max;
+rsort($sum);
+echo $sum[0] . ", " . $sum[1] . ", " . $sum[2] . "\n";
+echo $sum[0] + $sum[1] + $sum[2];
