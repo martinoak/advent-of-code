fd575345c3c1025df82dbace068208f98beda06c
Martin Dub <martin.dub@dek-cz.com>
2024-12-02 10:27:20 +0100

    2024-02 [1]

---
+++ 2024/2024-02.php
@@ -0,0 +1,45 @@
+<?php
+
+$input = file_get_contents('inputs/02.txt');
+
+$minOffset = 1;
+$maxOffset = 3;
+$safeCount = 0;
+
+foreach (explode("\n", $input) as $line) {
+    $levels = array_map('intval', explode(' ', $line));
+
+    if (checkSequence($levels, $minOffset, $maxOffset)) {
+        $safeCount++;
+    }
+}
+
+function checkSequence(array $row, int $minOffset, int $maxOffset): bool
+{
+    $previous = null;
+    $direction = null;
+
+    foreach ($row as $value) {
+        if ($previous !== null) {
+            $diff = $value - $previous;
+
+            if ($direction === null) {
+                $direction = $diff > 0 ? 'inc' : 'dec';
+            }
+
+            if (($direction === 'inc' && $diff <= 0) || ($direction === 'dec' && $diff >= 0)) {
+                return false;
+            }
+
+            if (abs($diff) < $minOffset || abs($diff) > $maxOffset) {
+                return false;
+            }
+        }
+
+        $previous = $value;
+    }
+
+    return true;
+}
+
+echo $safeCount . PHP_EOL;
