68c0c2346056884433e272b94e92e855e136fbb9
Martin Dub <martin.dub@dek-cz.com>
2025-02-06 08:49:36 +0100

    2015-05 [2]

--- 2015/2015-05.php
+++ 2015/2015-05.php
@@ -1,26 +1,42 @@
 <?php
 $input = file_get_contents('inputs/2015-05.txt');
-
 $input = explode("\n", $input);
 $nice = 0;
+
 foreach ($input as $line) {
-    $vowel = 0;
-    $substr = false;
-    $double = false;
-    if (str_contains($line, "ab") || str_contains($line, "cd") || str_contains($line, "pq") || str_contains($line, "xy")) {
-        $substr = true;
-    }
-    foreach (str_split($line) as $key2 => $value2) {
-        if ($value2 == "a" || $value2 == "e" || $value2 == "i" || $value2 == "o" || $value2 == "u") {
-            $vowel++;
-        }
-        if ($key2 > 0 && $value2 == $line[$key2 - 1]) {
-            $double = true;
-        }
-    }
-    if ($vowel >= 3 && $double && !$substr) {
+    if (isNice($line)) {
         $nice++;
     }
-    echo $line . " " . $nice . "\n";
 }
-echo $nice;
+
+function isNice($line): bool
+{
+    return hasPairTwice($line) && hasRepeatingLetterWithOneBetween($line);
+}
+
+function hasPairTwice($line): bool
+{
+    $pairs = [];
+    for ($i = 0; $i < strlen($line) - 1; $i++) {
+        $pair = $line[$i] . $line[$i + 1];
+        if (isset($pairs[$pair]) && $pairs[$pair] < $i - 1) {
+            return true;
+        }
+        if (!isset($pairs[$pair])) {
+            $pairs[$pair] = $i;
+        }
+    }
+    return false;
+}
+
+function hasRepeatingLetterWithOneBetween($line): bool
+{
+    for ($i = 0; $i < strlen($line) - 2; $i++) {
+        if ($line[$i] == $line[$i + 2]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+echo $nice . PHP_EOL;
