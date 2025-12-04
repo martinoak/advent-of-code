8d72ad71704630a53e49846689ebf1b3d1d0192e
Martin Dub <martin.dub@dek-cz.com>
2025-12-04 08:00:50 +0100

    2025-02 [1]

---
+++ 2025/2025-02.php
@@ -0,0 +1,22 @@
+<?php
+
+$input = file_get_contents(__DIR__ . '/inputs/02.txt');
+$sum = 0;
+$idRanges = explode(',', $input);
+
+foreach ($idRanges as $idRange) {
+    [$min, $max] = explode('-', $idRange);
+
+    foreach (range($min, $max) as $integer) {
+        if (isValid($integer)) {
+            $sum += $integer;
+        }
+    }
+}
+
+function isValid(int $integer): bool
+{
+    return preg_match('/^(\d+)(\1)$/', $integer) === 1;
+}
+
+echo $sum . PHP_EOL;
